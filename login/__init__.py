89038142a8a277974a92b4d7dbe4bb179718d944
enscogitans <37674855+enscogitans@users.noreply.github.com>
2023-01-07 19:14:16 +0200

    move code to main

--- login/__init__.py
+++ login/__init__.py
@@ -1,25 +1,3 @@
-import argparse
+from login.main import fill_parser, main
 
-from config import Config
-from login.login import login_tg, login_vk
-
-
-def fill_parser(parser: argparse.ArgumentParser) -> None:
-    subparsers = parser.add_subparsers(dest="app", required=True)
-
-    vk_parser = subparsers.add_parser("vk")
-    vk_parser.add_argument("--with-login", action="store_true", help="authorize using login and password")
-    vk_parser.add_argument("--show-password", action="store_true")
-
-    tg_parser = subparsers.add_parser("tg")
-    tg_parser.add_argument("--show-password", action="store_true")
-
-
-async def main(args: argparse.Namespace, config: Config) -> None:
-    match args.app:
-        case "vk":
-            login_vk(config.vk, with_login=args.with_login, hide_password=not args.show_password)
-        case "tg":
-            await login_tg(config.tg, hide_password=not args.show_password)
-        case app:
-            raise ValueError(f"Unexpected app {app}")
+__all__ = ["fill_parser", "main"]
